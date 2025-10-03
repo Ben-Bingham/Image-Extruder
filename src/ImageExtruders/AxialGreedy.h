2b6cbe928b4e44052231c691d3dbb08119207aa7
Ben-Bingham <ben@benbingham.ca>
2025-10-03 17:12:04 -0600

    Added comment

--- src/ImageExtruders/AxialGreedy.h
+++ src/ImageExtruders/AxialGreedy.h
@@ -2,6 +2,7 @@
 
 #include "ImageExtruder.h"
 
+// Attempts to expand in the x acis first, then towards the y axis, starts at (0,0).
 class AxialGreedy : public ImageExtruder {
 public:
 	Mesh ExtrudeImage(const Image& image) override;
